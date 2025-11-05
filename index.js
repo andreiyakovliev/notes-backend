63ccd97a9d70bcde8c30b84390164f9abf97bc7b
Andrii Yakovliev <andrii.iakovliev@gmail.com>
2025-11-05 17:28:12 +0200

    chore: deploy to production

--- index.js
+++ index.js
@@ -1,99 +1,123 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import morgan from 'morgan';
 
 const app = express()
-
 app.use(express.json())
-app.use(cors())
+morgan.token('body', request => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(morgan('tiny'))
 app.use(express.static('dist'))
 
-let notes = [
+let persons = [
     {
-        id: '1',
-        content: 'HTML is easy',
-        important: true
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: '2',
-        content: 'Browser can execute only JavaScript',
-        important: false
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: '3',
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-// const app = createServer((request, response) => {
-//     response.writeHead(200, { 'Content-type': 'application/json' })
-//     response.end(JSON.stringify(notes))
-// })
-
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+    response.send('<h1>Welcome to my world!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/info', (request, response) => {
+    const countPersons = persons.length
+
+    const date = new Date()
+
+    const offsetMinutes = -date.getTimezoneOffset()
+    const sign = offsetMinutes >= 0 ? '+' : '-'
+    const hours = String(Math.floor(Math.abs(offsetMinutes) / 60)).padStart(2, '0')
+    const minutes = String(Math.abs(offsetMinutes) % 60).padStart(2, '0')
+    const offset = `${sign}${hours}${minutes}`
+
+    const day = date.toDateString()
+    const time = date.toTimeString().slice(0, 8)
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+    response.send(
+        `<p>Phonebook has info for ${countPersons} people</p>
+        <p>${day} ${time} ${offset} ${timeZone}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
     } else {
         response.status(404).end()
     }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+
+    persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0 // найбільший ідентифікатор у поточному списку
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0
+const generateRandomId = (existingPersons) => {
+    let id
+    do {
+        id = Math.floor(Math.random() * 1_000_000_000) + 1
 
-    return String(maxId + 1)
+    } while (existingPersons.some(p => p.id === id));
+    return id
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(body);
+    console.log('Headers:', request.headers)
+    console.log('Body:', request.body)
 
-    if (!body.content) {
-        return response.status(400), json(
+    if (!body.name || !body.number) {
+        return response.status(400).json(
             {
-                error: 'content missing'
+                error: 'name or number missing'
             }
         )
     }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
+    if (persons.some(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
     }
 
-    console.log(notes)
 
-    notes = notes.concat(note)
+    const person = {
+        id: generateRandomId(persons),
+        name: body.name,
+        number: body.number
+    }
 
-    console.log(notes)
-
-    response.json(note);
-
+    persons = persons.concat(person)
+    response.json(person);
 })
 
-const PORT = process.env.PORT || 3001
-// app.listen(PORT)
-// console.log(`Server running on port ${PORT}`)
+
+const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 })
