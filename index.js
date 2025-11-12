f5a0d32c06236b86c7b0c6595cc1d567b66d6472
Andrii Yakovliev <andrii.iakovliev@gmail.com>
2025-11-12 19:05:07 +0200

    Use MongoDB instead of local data

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import express from 'express'
+import 'dotenv/config' // маэ бути першим ніж note
+import express, { request, response } from 'express'
 import mongoose from 'mongoose'
-import 'dotenv/config' // маэ бути першим ныж note
 import Note from './models/note.js'
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
+// app.use(requestLogger)
 
 const password = process.argv[2]
 
@@ -29,23 +31,23 @@ const password = process.argv[2]
 
 // const Note = mongoose.model('Note', noteSchema)
 
-let notes = [
-    {
-        id: '1',
-        content: 'HTML is easy',
-        important: true
-    },
-    {
-        id: '2',
-        content: 'Browser can execute only JavaScript',
-        important: false
-    },
-    {
-        id: '3',
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: '1',
+//         content: 'HTML is easy',
+//         important: true
+//     },
+//     {
+//         id: '2',
+//         content: 'Browser can execute only JavaScript',
+//         important: false
+//     },
+//     {
+//         id: '3',
+//         content: 'GET and POST are the most important methods of HTTP protocol',
+//         important: true
+//     }
+// ]
 
 // const app = createServer((request, response) => {
 //     response.writeHead(200, { 'Content-type': 'application/json' })
@@ -58,21 +60,30 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
     // response.json(notes)
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+    Note.find({})
+        .then(notes => {
+            response.json(notes)
+        })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        // .catch(error => {
+        //     console.log(error);
+        //     response.status(500).send({ error: 'malformatted id' })
+        // })
+        .catch(error => next(error)) // передаємо помилку, як параметр насутпній функції
+    // const id = request.params.id
+    // const note = notes.find(note => note.id === id)
 
+})
 
 app.delete('/api/notes/:id', (request, response) => {
     // const id = request.params.id
@@ -80,17 +91,16 @@ app.delete('/api/notes/:id', (request, response) => {
 
     // response.status(204).end()
 
-    Note.findById(request.params.id)
-        .then(note => {
-            response.json(note)
-            console.log(note);
-
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
 const generateId = () => {
-    const maxId = notes.length > 0 // найбільший ідентифікатор у поточному списку
-        ? Math.max(...notes.map(n => Number(n.id)))
+    const maxId = Note.length > 0 // найбільший ідентифікатор у поточному списку
+        ? Math.max(...Note.map(n => Number(n.id)))
         : 0
 
     return String(maxId + 1)
@@ -98,7 +108,10 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-    console.log(body);
+    console.log('Отримано body:', body);
+    console.log('Type of body:', typeof body);
+    console.log('body.content:', body.content);
+    console.log('body.important:', body.important);
 
     if (!body.content) {
         return response.status(400).json(
@@ -108,21 +121,69 @@ app.post('/api/notes', (request, response) => {
         )
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+        // id: generateId(),
+    })
 
-    console.log(notes)
+    // note = note.concat(note)
 
-    notes = notes.concat(note)
+    // console.log(note)
+    note.save()
+        .then(savedNote => {
+            console.log(savedNote);
 
-    console.log(notes)
-
-    response.json(note);
+            response.json(savedNote);
+        })
+    // response.json(note);
 
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+
+    // це теж саме, що вище
+    // const content = request.body.content
+    // const important = request.body.important
+
+    Note.findById(request.params.id)
+        .then(note => {
+            if (!note) {
+                return response.status(404).end()
+            }
+
+            note.content = content
+            note.important = important
+
+            return note.save().then((updatedNote) => {
+                response.json(updatedNote)
+            })
+        })
+        .catch(error => next(error))
+})
+
+// обробки непідтримуваних маршрутів
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// Обробники помилок Express
+
+const errorHandle = (error, request, response, next) => {
+    console.log(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandle)
 
 const PORT = process.env.PORT
 // app.listen(PORT)
